b88f558c8c5adb113a8b515823cb183dc4afebbd
Miguel Liezun <liezun.js@gmail.com>
2019-03-05 13:44:57 -0300

    Comento todo lo que no se usa

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -23,6 +23,8 @@ if (isset($this->params['breadcrumbs'])) {
 
 $empresa = Yii::$app->session->get('Parametros')['EMPRESA'];
 $logo = Yii::$app->session->get('Parametros')['LOGO'];
+
+$usuario = Yii::$app->user->identity;
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -56,22 +58,34 @@ $logo = Yii::$app->session->get('Parametros')['LOGO'];
                 </button>
                 <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto navbar-right-top">
-                        <li class="nav-item">
+                        <!--li class="nav-item">
                             <div id="custom-search" class="top-search-bar">
-                                <input class="form-control" type="text" placeholder="Search..">
+                                <input class="form-control" type="text" placeholder="Buscar..">
                             </div>
-                        </li>
-                        <li class="nav-item dropdown notification">
-                            <a class="nav-link nav-icons" href="#" id="navbarDropdownMenuLink1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-fw fa-bell"></i> <span class="indicator"></span></a>
+                        </li-->
+                        <!--li class="nav-item dropdown notification">
+                            <a
+                                class="nav-link nav-icons"
+                                href="#"
+                                id="navbarDropdownMenuLink1"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false">
+                                <i class="fas fa-fw fa-bell"></i>
+                                <span class="indicator"></span>
+                            </a>
                             <ul class="dropdown-menu dropdown-menu-right notification-dropdown">
                                 <li>
-                                    <div class="notification-title"> Notification</div>
+                                    <div class="notification-title"> Notificaciones</div>
                                     <div class="notification-list">
                                         <div class="list-group">
                                             <a href="#" class="list-group-item list-group-item-action active">
                                                 <div class="notification-info">
-                                                    <div class="notification-list-user-img"><img src="assets/images/avatar-2.jpg" alt="" class="user-avatar-md rounded-circle"></div>
-                                                    <div class="notification-list-user-block"><span class="notification-list-user-name">Jeremy Rakestraw</span>accepted your invitation to join the team.
+                                                    <div class="notification-list-user-img">
+                                                        <img src="assets/images/avatar-2.jpg" alt="" class="user-avatar-md rounded-circle">
+                                                    </div>
+                                                    <div class="notification-list-user-block">
+                                                        <span class="notification-list-user-name">Jeremy Rakestraw</span>accepted your invitation to join the team.
                                                         <div class="notification-date">2 min ago</div>
                                                     </div>
                                                 </div>
@@ -104,11 +118,11 @@ $logo = Yii::$app->session->get('Parametros')['LOGO'];
                                     </div>
                                 </li>
                                 <li>
-                                    <div class="list-footer"> <a href="#">View all notifications</a></div>
+                                    <div class="list-footer"> <a href="#">Ver todas</a></div>
                                 </li>
                             </ul>
-                        </li>
-                        <li class="nav-item dropdown connection">
+                        </li-->
+                        <!--li class="nav-item dropdown connection">
                             <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="fas fa-fw fa-th"></i> </a>
                             <ul class="dropdown-menu dropdown-menu-right connection-dropdown">
                                 <li class="connection-list">
@@ -136,20 +150,26 @@ $logo = Yii::$app->session->get('Parametros')['LOGO'];
                                     </div>
                                 </li>
                                 <li>
-                                    <div class="conntection-footer"><a href="#">More</a></div>
+                                    <div class="conntection-footer"><a href="#">Ver más</a></div>
                                 </li>
                             </ul>
-                        </li>
+                        </li-->
                         <li class="nav-item dropdown nav-user">
-                            <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="assets/images/avatar-1.jpg" alt="" class="user-avatar-md rounded-circle"></a>
+                            <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="far fa-user"></i>
+                            </a>
                             <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
                                 <div class="nav-user-info">
-                                    <h5 class="mb-0 text-white nav-user-name">John Abraham </h5>
-                                    <span class="status"></span><span class="ml-2">Available</span>
+                                    <h5 class="mb-0 text-white nav-user-name">
+                                        <?= "$usuario->Nombres $usuario->Apellidos" ?>
+                                    </h5>
+                                    <!--span class="status"></span><span class="ml-2">Disponible</span-->
                                 </div>
-                                <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a>
-                                <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Setting</a>
-                                <a class="dropdown-item" href="#"><i class="fas fa-power-off mr-2"></i>Logout</a>
+                                <!--a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a>
+                                <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Setting</a-->
+                                <a class="dropdown-item" href="/usuarios/logout">
+                                    <i class="fas fa-power-off mr-2"></i>Cerrar sesión
+                                </a>
                             </div>
                         </li>
                     </ul>
