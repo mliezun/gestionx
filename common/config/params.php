c26cd14ed175245d80ae80683d32c484d58db9c9
Miguel Liezun <liezun.js@gmail.com>
2019-09-21 22:23:43 -0300

    Mejoras en LineasIngreso y otras frutas

--- common/config/params.php
+++ common/config/params.php
@@ -1,4 +1,14 @@
 <?php
 return [
     'bsVersion' => '4.x',
+    'maskMoneyOptions' => [
+        'prefix' => '$ ',
+        'suffix' => '',
+        'affixesStay' => false,
+        'thousands' => '.',
+        'decimal' => ',',
+        'precision' => 2, 
+        'allowZero' => false,
+        'allowNegative' => false
+    ]
 ];
