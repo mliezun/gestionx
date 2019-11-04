b5b68f14f63137d2ec019c82e3ce452e016da21a
Miguel Liezun <liezun.js@gmail.com>
2019-11-03 21:44:14 -0300

    Carga de articulos y aumentos de precios

--- common/models/Proveedores.php
+++ common/models/Proveedores.php
@@ -11,6 +11,9 @@ class Proveedores extends Model
     public $Proveedor;
     public $Descuento;
     public $Estado;
+    // Derivados
+    public $Aumento;
+    public $Archivo;
 
     const ESTADOS = [
         'A' => 'Activo',
@@ -19,6 +22,8 @@ class Proveedores extends Model
 
     const SCENARIO_ALTA = 'alta';
     const SCENARIO_EDITAR = 'editar';
+    const SCENARIO_AUMENTO = 'aumento';
+
 
     /**
      * Reglas para validar los formularios.
@@ -34,6 +39,8 @@ class Proveedores extends Model
             [['IdEmpresa', 'Proveedor', 'Descuento'], 'required', 'on' => self::SCENARIO_ALTA],
             // Editar
             [['IdProveedor', 'Proveedor', 'Descuento'], 'required', 'on' => self::SCENARIO_EDITAR],
+            // Aumento
+            [['IdProveedor', 'Aumento'], 'required', 'on' => self::SCENARIO_AUMENTO],
             // Safe
             [['IdProveedor', 'IdEmpresa', 'Proveedor', 'Estado'], 'safe'],
         ];
@@ -42,7 +49,7 @@ class Proveedores extends Model
     /**
      * Permite instaciar un proveedor desde la base de datos.
      * xsp_dame_proveedor
-     * 
+     *
      */
     public function Dame()
     {
@@ -59,6 +66,62 @@ class Proveedores extends Model
         $this->attributes = $res;
         
         return $res;
+    }
+
+    /**
+     * Permite aplicar un aumento a todos los artículos de un proveedor. Devuelve OK o el mensaje de error en Mensaje.
+     * xsp_aplicar_aumento_proveedor
+     *
+     */
+    public function AplicarAumento()
+    {
+        $sql = "call xsp_aplicar_aumento_proveedor( :token, :idproveedor, :aumento, :IP, :userAgent, :app)";
+
+        $query = Yii::$app->db->createCommand($sql);
+        
+        $query->bindValues([
+            ':token' => Yii::$app->user->identity->Token,
+            ':IP' => Yii::$app->request->userIP,
+            ':userAgent' => Yii::$app->request->userAgent,
+            ':app' => Yii::$app->id,
+            ':idproveedor' => $this->IdProveedor,
+            ':aumento' => $this->Aumento
+        ]);
+
+        return $query->queryScalar();
+    }
+
+    /**
+     * Permite hacer un alta/modifica masivo de artículos de un proveedor. Devuelve OK o el mensaje de error en Mensaje.
+     * xsp_cargar_articulos_proveedor
+     *
+     */
+    public function CargarArticulos()
+    {
+        $archivo = file_get_contents($this->Archivo->tempName, 'r');
+        $archivo = "Articulo,Codigo,Descripcion,PrecioCosto,IVA\n" . $archivo;
+        file_put_contents($this->Archivo->tempName, $archivo);
+
+        $csv = array_map('str_getcsv', file($this->Archivo->tempName));
+        array_walk($csv, function (&$a) use ($csv) {
+            $a = array_combine($csv[0], $a);
+        });
+        array_shift($csv);
+
+        $sql = "call xsp_cargar_articulos_proveedor( :token, :idproveedor, :articulos, :IP, :userAgent, :app)";
+
+        $query = Yii::$app->db->createCommand($sql);
+        
+        $query->bindValues([
+            ':token' => Yii::$app->user->identity->Token,
+            ':IP' => Yii::$app->request->userIP,
+            ':userAgent' => Yii::$app->request->userAgent,
+            ':app' => Yii::$app->id,
+            ':idproveedor' => $this->IdProveedor,
+            ':articulos' => json_encode($csv)
+        ]);
+
+        return $query->queryScalar();
     }
 
     /**
@@ -106,8 +169,8 @@ class Proveedores extends Model
     }
 
     /*
-	* Permite listar el historial de descuentos de un proveedor.
-	*/
+    * Permite listar el historial de descuentos de un proveedor.
+    */
     public function ListarHistorialDescuentos()
     {
         $sql = 'CALL xsp_listar_historial_proveedor( :id)';
