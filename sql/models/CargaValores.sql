e06bef035cc006bdc18c70a687027d292fc70a94
Miguel Liezun <liezun.js@gmail.com>
2020-06-09 14:43:33 -0300

    vagrant initial configuration

--- sql/models/CargaValores.sql
+++ sql/models/CargaValores.sql
@@ -357,7 +357,6 @@ INSERT INTO PermisosRolGenerico VALUES(81, 1);
 INSERT INTO PermisosRolGenerico VALUES(82, 1);
 INSERT INTO PermisosRolGenerico VALUES(83, 1);
 INSERT INTO PermisosRolGenerico VALUES(84, 1);
-INSERT INTO PermisosRolGenerico VALUES(84, 1);
 INSERT INTO PermisosRolGenerico VALUES(85, 1);
 INSERT INTO PermisosRolGenerico VALUES(86, 1);
 INSERT INTO PermisosRolGenerico VALUES(87, 1);
@@ -406,7 +405,6 @@ INSERT INTO PermisosRolGenerico VALUES(129, 1);
 INSERT INTO PermisosRolGenerico VALUES(130, 1);
 INSERT INTO PermisosRolGenerico VALUES(131, 1);
 INSERT INTO PermisosRolGenerico VALUES(132, 1);
-INSERT INTO PermisosRolGenerico VALUES(132, 1);
 INSERT INTO PermisosRolGenerico VALUES(133, 1);
 INSERT INTO PermisosRolGenerico VALUES(134, 1);
 INSERT INTO PermisosRolGenerico VALUES(135, 1);
@@ -449,7 +447,6 @@ INSERT INTO PermisosRolGenerico VALUES(80, 2);
 INSERT INTO PermisosRolGenerico VALUES(81, 2);
 INSERT INTO PermisosRolGenerico VALUES(82, 2);
 INSERT INTO PermisosRolGenerico VALUES(83, 2);
-INSERT INTO PermisosRolGenerico VALUES(83, 2);
 INSERT INTO PermisosRolGenerico VALUES(85, 2);
 INSERT INTO PermisosRolGenerico VALUES(86, 2);
 INSERT INTO PermisosRolGenerico VALUES(87, 2);
@@ -487,6 +484,85 @@ INSERT INTO PermisosRolGenerico VALUES(134, 2);
 INSERT INTO PermisosRolGenerico VALUES(135, 2);
 INSERT INTO PermisosRolGenerico VALUES(136, 2);
 INSERT INTO PermisosRolGenerico VALUES(145, 2);
+
+-- Genero datos de empresa inicial
+DROP PROCEDURE IF EXISTS `xsp_generar_datos_empresa`;
+DELIMITER $$
+CREATE PROCEDURE `xsp_generar_datos_empresa`(pIdEmpresa int, out pMensaje varchar(255))
+BEGIN
+	/*
+    Permite generar los roles y parámetros de una empresa, a partir de sus módulos y los roles genéricos.
+    */
+    DECLARE pIndice int default 1;
+	-- Manejo de error en la transacción
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		-- show errors;
+		SET pMensaje = 'Error en la transacción interna. Contáctese con el administrador.';
+	END;
+
+    SET pMensaje = NULL;
+    
+    SET @fila = 0;
+    DROP TEMPORARY TABLE IF EXISTS tmp_parametros_empresa;
+    CREATE TEMPORARY TABLE tmp_parametros_empresa ENGINE = MEMORY
+		SELECT		@fila := @fila + 1 Fila, p.Parametro, p.IdModulo, p.DameValor
+        FROM		Parametros p
+        INNER JOIN	ModulosEmpresas me USING (IdModulo)
+        WHERE		me.IdEmpresa = pIdEmpresa;
+
+        
+	-- DameValor recibe @pIdEmpresa y retorna el valor en @pValor
+	SET @pIdEmpresa = pIdEmpresa;
+    
+	WHILE pIndice <= @fila DO
+        call xsp_eval((SELECT DameValor FROM tmp_parametros_empresa WHERE Fila = pIndice));
+        
+		UPDATE	tmp_parametros_empresa
+        SET		DameValor = @pValor
+        WHERE	Fila = pIndice;
+        
+        SET pIndice = pIndice + 1;
+    END WHILE;
+
+    DROP TEMPORARY TABLE IF EXISTS tmp_roles_permisosrol_genericos;
+    CREATE TEMPORARY TABLE tmp_roles_permisosrol_genericos ENGINE = MEMORY
+        SELECT			rg.IdRolGenerico, rg.Rol, rg.Estado, rg.Observaciones, prg.IdPermiso
+        FROM			ModulosEmpresas me
+        INNER JOIN		Permisos p USING(IdModulo)
+        INNER JOIN		PermisosRolGenerico prg USING(IdPermiso)
+        INNER JOIN		RolesGenericos rg USING(IdRolGenerico)
+        WHERE			me.IdEmpresa = pIdEmpresa;
+
+    INSERT INTO Roles
+    SELECT      0, Rol, Estado, Observaciones, pIdEmpresa
+    FROM        tmp_roles_permisosrol_genericos
+    GROUP BY    IdRolGenerico, Rol, Estado, Observaciones;
+
+    DROP TEMPORARY TABLE IF EXISTS tmp_permisosrol;
+    CREATE TEMPORARY TABLE tmp_permisosrol ENGINE = MEMORY
+        SELECT  DISTINCT Rol, IdPermiso
+        FROM    tmp_roles_permisosrol_genericos;
+
+    INSERT INTO		PermisosRol
+    SELECT			IdPermiso, (SELECT IdRol FROM Roles WHERE IdEmpresa = pIdEmpresa AND Rol = (SELECT Rol FROM tmp_permisosrol WHERE IdPermiso=t.IdPermiso LIMIT 1))
+    FROM            tmp_roles_permisosrol_genericos t
+    GROUP BY        IdPermiso;
+
+    INSERT INTO		ParametroEmpresa
+    SELECT			t.Parametro, pIdEmpresa, t.IdModulo, t.DameValor
+    FROM			tmp_parametros_empresa t;
+
+    INSERT INTO Usuarios VALUES (0, (SELECT MIN(IdRol) FROM Roles WHERE IdEmpresa = pIdEmpresa), 'Admin', 'Admin', 'admin', md5('admin'), '', CONCAT('admin@example.com'), '0', NOW(), NOW(), 'N', 'A', NULL, pIdEmpresa);
+
+    SET pMensaje = 'OK';
+    
+    DROP TEMPORARY TABLE IF EXISTS tmp_parametros_empresa;
+    DROP TEMPORARY TABLE IF EXISTS tmp_roles_permisosrol_genericos;
+    DROP TEMPORARY TABLE IF EXISTS tmp_permisosrol;
+END$$
+DELIMITER ;
+
 
 call xsp_generar_datos_empresa(1, @mensaje);
 SELECT @mensaje AltaEmpresa;
