e9d86522979d1a959ecfba2e2ba4cba9258aa091
Miguel Liezun <liezun.js@gmail.com>
2020-07-24 14:53:28 -0300

    fix: informes asset add css

--- web/backend/views/informes/index.php
+++ web/backend/views/informes/index.php
@@ -43,24 +43,6 @@ Yii::info($reportes);
     <?php endforeach; ?>
 </div>
 
-<style>
-.overlay {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0,0,0,0.5);
-  z-index: 999999;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
-
 <?php if ($reporte != null): ?>
 
 <div class="box" id="informes">
